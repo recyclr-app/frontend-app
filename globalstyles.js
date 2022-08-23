fbdf21e4263d95d2def9549f0ce601c345691ddc
Stephen Wong <wongstephenky@gmail.com>
2022-08-23 16:43:07 -0700

    Stephen (#1)
    * added icons
    
    * Aug 23 Homepage Build
    
    Co-authored-by: luuxe <luxelily@gmail.com>

---
+++ globalstyles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const colors = {
+  black: "#000000",
+  blob: "#D0F2E2",
+  green1: "#8ADEB7",
+  green2: "#6ED3A6",
+  green3: "#84CD6D",
+  green4: "#1AB858",
+};
+
+export const global = StyleSheet.create({});
