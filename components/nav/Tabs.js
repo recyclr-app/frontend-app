f8aefce4203107d0c37911ab5f0daa407162f501
luuxe <luxelily@gmail.com>
2022-08-24 10:46:18 -0500

    Luxe - nav bar/camera/upload

--- components/nav/Tabs.js
+++ components/nav/Tabs.js
@@ -3,6 +3,8 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Upload from "../Upload";
 import CameraPhoto from "../CameraPhoto";
 import Main from '../Main'
+import { View, Image, Text } from 'react-native'
+// import cameraIcon from '../../assets/icons/camera.png'
 
 const Tab = createBottomTabNavigator()
 
@@ -10,12 +12,67 @@ const Tabs = () => {
     return (
         <Tab.Navigator
             screenOptions={{
-                headerShown: false
-            }}
-        >
-            <Tab.Screen name="Main" component={Main} />
-            <Tab.Screen name="Take a Photo" component={CameraPhoto} />
-            <Tab.Screen name="Upload" component={Upload} />
+                headerShown: false,
+                tabBarShowLabel: true,
+                tabBarActiveTintColor: '#1AB858'
+            }}>
+            <Tab.Screen
+                name="Main"
+                component={Main}
+                options={{
+                    tabBarIcon: ({ focused }) => (
+                        <View>
+                            <Image
+                                source={require('../../assets/icons/home.png')}
+                                resizeMode='contain'
+                                style={{
+                                    width: 25,
+                                    height: 25,
+                                    tintColor: focused ? '#1AB858' : 'black'
+                                }}
+                            />
+                        </View>
+                    )
+                }} 
+            />
+            <Tab.Screen
+                name="Take a Photo"
+                component={CameraPhoto}
+                options={{
+                    tabBarIcon: ({ focused }) => (
+                        <View>
+                            <Image
+                                source={require('../../assets/icons/camera.png')}
+                                resizeMode='contain'
+                                style={{
+                                    width: 25,
+                                    height: 25,
+                                    tintColor: focused ? '#1AB858' : 'black'
+                                }}
+                            />
+                        </View>
+                    )
+                }}    
+            />
+            <Tab.Screen
+                name="Upload"
+                component={Upload}
+                options={{
+                    tabBarIcon: ({focused}) => (
+                        <View>
+                            <Image
+                                source={require('../../assets/icons/image-gallery.png')}
+                                resizeMode='contain'
+                                style={{
+                                    width: 25,
+                                    height: 25,
+                                    tintColor: focused ? '#1AB858' : 'black'
+                                }}
+                            />
+                        </View>
+                    )
+                }} 
+            />
         </Tab.Navigator>
     )
 }
