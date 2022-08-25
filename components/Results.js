70b918f0c38a9cb1d58b09202a409caf69fe2f22
luuxe <luxelily@gmail.com>
2022-08-25 17:04:22 -0500

    successfully passed cv results state to Results component and rendered on screen

--- components/Results.js
+++ components/Results.js
@@ -1,15 +1,19 @@
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
 import React from "react";
 
-export default function Results({ cvResults }) {
+export default function Results({ route }) {
   // console.log(cvResults.name)
-
+  const { cvResults } = route.params;
+  console.log(cvResults.uri)
   return (
     <SafeAreaView>
       <Text>In Results</Text>
-      {/* <Text>{cvResults.name}</Text> */}
+      <Image source={{ uri: cvResults.url }} style={{ width: 300, height: 300 }} />
+      <Text>{cvResults.name}</Text>
     </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+
+});
