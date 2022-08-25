3c3b5d51f2c69b0417b665bbec1a3eb82e19b80b
Luxe Hahn <104602440+luuxe@users.noreply.github.com>
2022-08-25 14:52:37 -0500

    created nested stack under upload and cameraphoto (#6)

---
+++ components/Results.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
+import React from 'react'
+
+export default function Results({ cvResults }) {
+console.log(cvResults.name)
+
+  return (
+    <SafeAreaView>
+      <Text>In Results</Text>
+      <Text>{cvResults.name}</Text>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({})
