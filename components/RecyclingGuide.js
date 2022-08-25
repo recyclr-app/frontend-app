abab918650b9aa5a16003315d89746f68f820c34
Stephen Wong <wongstephenky@gmail.com>
2022-08-25 15:36:58 -0700

    Recycling Guide Url

--- components/RecyclingGuide.js
+++ components/RecyclingGuide.js
@@ -1,11 +1,15 @@
-import { Text, View } from "react-native";
+import { Text, View, TouchableOpacity, Linking } from "react-native";
 import React, { Component } from "react";
 
 export class RecyclingGuide extends Component {
   render() {
+    const uriEpa =
+      "https://www.bing.com/search?q=national+recycling+resources&cvid=ca9c4ce0e3c2475f9fef7094bcfa50e7&aqs=edge..69i57j0j69i64.6231j0j4&FORM=ANAB01&PC=U531";
     return (
       <View>
-        <Text>RecyclingGuide</Text>
+        <TouchableOpacity onPress={() => Linking.openURL(uriEpa)}>
+          <Text>EPA</Text>
+        </TouchableOpacity>
       </View>
     );
   }
