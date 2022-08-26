187faabb528f1b844fe3ac8ad63f13d3f3b4a235
Stephen Wong <wongstephenky@gmail.com>
2022-08-25 21:41:41 -0700

    appinfo and recycle guide (#10)

--- components/AppInfo.js
+++ components/AppInfo.js
@@ -1,29 +1,79 @@
-import { View, Text, Image } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  ScrollView,
+  SafeAreaView,
+} from "react-native";
 import React from "react";
+import { colors } from "../globalstyles";
 
 const AppInfo = () => {
   return (
-    <View>
-      <Image source={require("../assets/icons/recycle2.png")} />
-      <Text>Built with love from Austin and Seattle</Text>
-      <Text>
-        Recycle is a free app developed with sustainability in mind. Bring the
-        world a step closer to being a better place by helping others live more
-        sustainable lives by encouraging people to recycle. The goal of this app
-        is to make the world a better place through awareness, education and
-        conservation. It's designed to help you know what you can recycle and
-        how to recycle it as well as other information on sustainability.
-        Individuals of all ages will be able to contribute in keeping our planet
-        green. The initial concept was to make it easy for consumers to recycle
-        waste products. We want people to be more mindful of their impact on the
-        environment so they can continue living authentically in the face of
-        challenging circumstances. There are over 700 million tons of trash
-        thrown into our landfills each year. We want to help you recycle what
-        you throw away. With Recycler you can get all the information you need
-        on your recyclables, in a clean, simple and visually appealing way.
-      </Text>
-    </View>
+    <SafeAreaView>
+      <ScrollView>
+        <View style={styles.container}>
+          <Image
+            source={require("../assets/icons/recycle2.png")}
+            style={styles.logo}
+          />
+          <Text style={styles.title}>ryclr app </Text>
+          <Text style={styles.subtitle}>
+            Built with love from Austin and Seattle, {"\n"}©{" "}
+            {new Date().getFullYear()}
+          </Text>
+
+          <Text style={styles.textBody}>
+            recyclr is a free app developed with sustainability in mind. The
+            goal of this app is to make the world a better place through
+            awareness, education and conservation. {"\n"} {"\n"}It's designed to
+            help you know what you can recycle and how to recycle it as well as
+            other information on sustainability. Individuals of all ages will be
+            able to contribute in keeping our planet green. {"\n"} {"\n"}The
+            initial concept was to make it easy for consumers to recycle waste
+            products. We want people to be more mindful of their impact on the
+            environment so they can continue living authentically in the face of
+            challenging circumstances. {"\n"} {"\n"}There are over 700 million
+            tons of trash thrown into our landfills each year. We want to help
+            you recycle what you throw away. With recyclr you can get all the
+            information you need on your recyclables, in a clean, simple and
+            visually appealing way.
+          </Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
-
+const styles = StyleSheet.create({
+  container: {
+    marginLeft: 20,
+    marginRight: 20,
+  },
+  logo: {
+    aspectRatio: 1,
+    height: 175,
+    margin: "auto",
+    marginBottom: 20,
+    marginLeft: "auto",
+    marginRight: "auto",
+    marginTop: 20,
+    resizeMode: "cover",
+  },
+  title: {
+    textAlign: "center",
+    fontSize: "30",
+  },
+  subtitle: {
+    textAlign: "center",
+    margin: 20,
+    fontWeight: "200",
+  },
+  textBody: {
+    letterSpacing: 1,
+    lineHeight: 25,
+    fontSize: 20,
+    fontWeight: "200",
+  },
+});
 export default AppInfo;
--- components/RecyclingGuide.js
+++ components/RecyclingGuide.js
@@ -1,18 +1,74 @@
-import { Text, View, TouchableOpacity, Linking } from "react-native";
+import {
+  Text,
+  View,
+  Linking,
+  SafeAreaView,
+  Button,
+  StyleSheet,
+} from "react-native";
 import React, { Component } from "react";
-
+import { colors } from "../globalstyles";
 export class RecyclingGuide extends Component {
   render() {
-    const uriEpa =
-      "https://www.bing.com/search?q=national+recycling+resources&cvid=ca9c4ce0e3c2475f9fef7094bcfa50e7&aqs=edge..69i57j0j69i64.6231j0j4&FORM=ANAB01&PC=U531";
+    const uriEpa = "https://www.epa.gov/recycle";
+    const uriWM =
+      "https://app.copy.ai/projects/7182734?tab=results&tool=ProductDescriptions";
     return (
-      <View>
-        <TouchableOpacity onPress={() => Linking.openURL(uriEpa)}>
-          <Text>EPA</Text>
-        </TouchableOpacity>
-      </View>
+      <SafeAreaView>
+        <View style={styles.container}>
+          <Text style={styles.textTitle}>Additional Resources</Text>
+          <View style={styles.articleContainer}>
+            <Text style={styles.textArticle}>
+              U.S. Environmental Protection Agency
+            </Text>
+            <Text style={styles.textBody}>
+              By reducing, reusing, and recycling, you can save money and
+              natural resources and help keep our planet beautiful by reducing
+              waste.
+            </Text>
+            <Button
+              onPress={() => Linking.openURL(uriEpa)}
+              title="Visit Site"
+            />
+          </View>
+          <View style={styles.articleContainer}>
+            <Text style={styles.textArticle}>
+              Waste Management Recycling 101
+            </Text>
+            <Text style={styles.textBody}>
+              Recycling is the key to protecting our planet. But we need your
+              help to make sure everything that enters your recycling bin finds
+              a second life: glass, plastic, cardboard and more.
+            </Text>
+            <Button onPress={() => Linking.openURL(uriWM)} title="Visit Site" />
+          </View>
+        </View>
+      </SafeAreaView>
     );
   }
 }
 
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+  },
+  textTitle: {
+    fontSize: 15,
+    letterSpacing: 1,
+    marginTop: 20,
+    marginBottom: 40,
+    fontWeight: "300",
+  },
+  textArticle: {
+    fontSize: 20,
+    marginBottom: 20,
+  },
+  textBody: {
+    fontSize: 15,
+    marginBottom: 10,
+  },
+  articleContainer: {
+    marginBottom: 50,
+  },
+});
 export default RecyclingGuide;
