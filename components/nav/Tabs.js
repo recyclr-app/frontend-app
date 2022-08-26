fc20ff17fc881b5f654e92882876531224f0320f
luuxe <luxelily@gmail.com>
2022-08-26 13:02:58 -0500

    route from results to user page, updated a lot of styling on camera, upload, results, and tab bar

--- components/nav/Tabs.js
+++ components/nav/Tabs.js
@@ -1,12 +1,17 @@
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import {
+  View,
+  Image,
+  Text
+} from "react-native";
 import Upload from "../Upload";
 import CameraPhoto from "../CameraPhoto";
 import HomeScreen from "../HomeScreen";
-import { View, Image, Text } from "react-native";
-// import cameraIcon from '../../assets/icons/camera.png'
 import UserHome from "../UserHome";
 import Results from "../Results";
+import UserMenu from "../UserMenu";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Ionicons } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
@@ -19,8 +24,12 @@ function UploadStack() {
     }}
     >
       <Stack.Screen name="UploadStack" component={Upload} />
-      <Stack.Screen name="Results" component={Results} />
+      {/* <Stack.Group screenOptions={{ presentation: 'modal' }} > */}
+        <Stack.Screen name="Results" component={Results} />
+      {/* </Stack.Group> */}
       <Stack.Screen name="OpenCamera" component={CameraPhoto} />
+      <Stack.Screen name="Achievement Page" component={UserMenu} />
+      
     </Stack.Navigator>
   );
 }
@@ -45,6 +54,21 @@ const Tabs = () => {
         headerShown: false,
         tabBarShowLabel: true,
         tabBarActiveTintColor: "#1AB858",
+        tabBarStyle: {
+          width: '90%',
+          height: 70,
+          margin: 25,
+          paddingTop: 12,
+          paddingBottom: 12,
+          position: 'absolute',
+          borderRadius: 50,
+          shadowColor: "#171717",
+          shadowOffset: { width: -2, height: 4 },
+          shadowOpacity: 0.1,
+          shadowRadius: 3,
+          alignItems: 'center',
+          justifyContent: 'space-between'
+        }
       }}
     >
       <Tab.Group>
@@ -67,32 +91,14 @@ const Tabs = () => {
             ),
           }}
         />
-        {/* <Tab.Screen
-          name="Camera"
-          component={CameraStack}
-          options={{
-            tabBarIcon: ({ focused }) => (
-              <View>
-                <Image
-                  source={require("../../assets/icons/camera.png")}
-                  resizeMode="contain"
-                  style={{
-                    width: 25,
-                    height: 25,
-                    tintColor: focused ? "#1AB858" : "black",
-                  }}
-                />
-              </View>
-            ),
-          }}
-        /> */}
         <Tab.Screen
           name="Add an Item"
           component={UploadStack}
           options={{
             tabBarIcon: ({ focused }) => (
               <View>
-                <Image
+                <Ionicons name='add-outline' size={30} />
+                {/* <Image
                   source={require("../../assets/icons/image-gallery.png")}
                   resizeMode="contain"
                   style={{
@@ -100,7 +106,7 @@ const Tabs = () => {
                     height: 25,
                     tintColor: focused ? "#1AB858" : "black",
                   }}
-                />
+                /> */}
               </View>
             ),
           }}
