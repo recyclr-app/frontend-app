1e1bf48da85dcc01b8de7826d04779f15f5471c5
Stephen Wong <wongstephenky@gmail.com>
2022-08-26 23:45:21 -0700

    fixed history

--- components/UserHistory.js
+++ components/UserHistory.js
@@ -4,43 +4,60 @@ import axios from "axios";
 
 export default function UserHistory() {
   const [historyData, setHistoryData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  const getHistory = async (userId = "630992c820fc61d17c3faf20") => {
-    const res = await axios.get(
-      "https://relievedmint.herokuapp.com/users/" + userId
-    );
-    setHistoryData(res.data.history);
-    console.log(res.data.history);
-    console.log(Math.random() * 100);
-    console.log("------------------------------");
-  };
+  useEffect(() => {
+    async function fetchData(userId = "630992c820fc61d17c3faf20") {
+      try {
+        const res = await axios.get(
+          "https://relievedmint.herokuapp.com/users/" + userId
+        );
+        setHistoryData(res.data.history);
+        console.log(res.data.history);
+        console.log(Math.random() * 100);
+        setLoading(false);
+        console.log("------------------------------");
+      } catch (err) {
+        console.error(error);
+      }
+    }
+    fetchData();
+  }, []);
 
-  useEffect(() => getHistory, []);
-
-  return (
-    <SafeAreaView>
-      <ScrollView>
+  if (loading) {
+    return (
+      <SafeAreaView>
         <View>
-          <Text>History Title Placeholder</Text>
-          {historyData.map((item) => (
-            <View key={item._id}>
-              <View>
-                <Image
-                  source={{ uri: item.image }}
-                  style={{ width: 100, height: 100 }}
-                />
-              </View>
-              <View>
-                <Text>{item.label}</Text>
-                <Text>{item.createdAt}</Text>
-                <Text>{item.label}</Text>
-                <Text>Delete Placeholder</Text>
-              </View>
-            </View>
-          ))}
-          <Text>Clear History Placeholder</Text>
+          <Text>Loading...</Text>
         </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+      </SafeAreaView>
+    );
+  } else {
+    return (
+      <SafeAreaView>
+        <ScrollView>
+          <View>
+            <Text>History Title Placeholder</Text>
+            {historyData.map((item) => (
+              <View key={item._id}>
+                <View>
+                  <Image
+                    source={{ uri: item.image }}
+                    style={{ width: 100, height: 100 }}
+                  />
+                </View>
+                <View>
+                  <Text>{item.label}</Text>
+                  <Text>{item.createdAt}</Text>
+                  <Text>{item.label}</Text>
+                  <Text>Delete Placeholder</Text>
+                </View>
+              </View>
+            ))}
+            <Text>Clear History Placeholder</Text>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    );
+  }
 }
