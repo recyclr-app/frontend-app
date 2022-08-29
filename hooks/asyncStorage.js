5b0fdff9dcd7637177e822fb0d7c1d02bd3d0e2f
Stephen Wong <wongstephenky@gmail.com>
2022-08-29 07:19:46 -0700

    cleaned files

--- hooks/asyncStorage.js
+++
@@ -1,8 +0,0 @@
-export const getAsyncStorage = async () => {
-  try {
-    const getStorage = await AsyncStorage.getItem("auth");
-    return JSON.parse(getStorage)
-  } catch (err) {
-    console.log(err);
-  }
-}
