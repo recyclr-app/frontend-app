fb4a98952e07c0ad56588d63fe5068e7b88c2dfa
luuxe <luxelily@gmail.com>
2022-08-29 09:37:19 -0500

    merge conflicts resolved

--- components/UserHistory.js
+++ components/UserHistory.js
@@ -25,6 +25,7 @@ export default function UserHistory() {
   const [loading, setLoading] = useState(true);
   const [localData, setLocalData] = useState({ token: "", id: "" });
 
+  
   useEffect(() => {
     const getLocalData = async () => {
       try {
