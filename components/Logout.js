0aea39f9483233553762c072c6b68d96cb3275c9
Stephen Wong <wongstephenky@gmail.com>
2022-08-29 09:30:25 -0700

    completed settings page (#22)

--- components/Logout.js
+++ components/Logout.js
@@ -1,8 +1,10 @@
 import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useNavigation } from "@react-navigation/native";
 
-export default function Logout() {
+export default function Logout(props) {
+  const navigation = useNavigation();
   const handleLogout = async () => {
     try {
       AsyncStorage.clear();
@@ -10,14 +12,15 @@ export default function Logout() {
         "auth",
         JSON.stringify({ token: "", id: "" })
       );
+      navigation.navigate(props.component);
     } catch (err) {
       console.log(err);
     }
   };
   return (
     <View>
-      <TouchableOpacity onPress={handleLogout}>
-        <Text>Log out</Text>
+      <TouchableOpacity onPress={handleLogout} style={props.buttonContainer}>
+        <Text style={props.buttonText}>Log out</Text>
       </TouchableOpacity>
     </View>
   );
--- components/Settings.js
+++ components/Settings.js
@@ -1,12 +1,44 @@
-import { SafeAreaView, Text } from "react-native";
+import { SafeAreaView, Text, StyleSheet, View } from "react-native";
 import React from "react";
+import Logout from "./Logout";
+import { colors } from "../globalstyles";
 
 const Settings = () => {
   return (
     <SafeAreaView>
-      <Text>Sign out</Text>
+      <View style={styles.pageContainer}>
+        <Text style={styles.pageTitle}>Settings{"\n"}</Text>
+        <Logout
+          component="UserMenu"
+          buttonContainer={styles.logout}
+          buttonText={styles.logoutText}
+        />
+      </View>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  pageContainer: {
+    margin: 16,
+    marginTop: 40,
+  },
+  pageTitle: {
+    fontSize: 30,
+    fontWeight: "600",
+  },
+  logout: {
+    backgroundColor: colors.green2,
+    padding: 15,
+    borderRadius: 100,
+    textAlign: "center",
+  },
+  logoutText: {
+    textAlign: "center",
+    fontWeight: "600",
+    color: "#ffffff",
+    letterSpacing: 1.25,
+  },
+});
 
 export default Settings;
